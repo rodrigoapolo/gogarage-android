241788c58ae54c09e9fbe6d34aef2c17df3ac056
rodrigoapolo <rodrigoapolodev@gmail.com>
2023-02-26 18:43:33 -0300

    layout regidter add strings

---
+++ app/src/main/java/com/rodrigoapolo/gogarage/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.rodrigoapolo.gogarage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        supportActionBar?.hide()
+    }
+}
