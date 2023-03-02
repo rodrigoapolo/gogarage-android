5d88bfdcfe1cc63f3ad35d4530cc9d223e9af890
Gabriel Hora <gabrielhoradeveloper@gmail.com>
2023-03-02 20:57:14 -0300

    intent lets login for register

--- app/src/main/java/com/rodrigoapolo/gogarage/LoginActivity.kt
+++ app/src/main/java/com/rodrigoapolo/gogarage/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.rodrigoapolo.gogarage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rodrigoapolo.gogarage.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        binding.textRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        return setContentView(binding.root)
     }
 }
