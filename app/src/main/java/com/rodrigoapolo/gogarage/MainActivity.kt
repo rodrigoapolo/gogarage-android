4073535933a804e36c99a4368bc3af920fd9df4f
Gabriel Hora <gabrielhoradeveloper@gmail.com>
2023-03-02 20:53:28 -0300

    intent lets started for login

--- app/src/main/java/com/rodrigoapolo/gogarage/MainActivity.kt
+++ app/src/main/java/com/rodrigoapolo/gogarage/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rodrigoapolo.gogarage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rodrigoapolo.gogarage.databinding.ActivityMainBinding
@@ -11,6 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.btnLetsStarted.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
         return setContentView(binding.root)
     }
 }
