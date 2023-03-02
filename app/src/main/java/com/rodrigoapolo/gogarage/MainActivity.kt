1d3d48213889b3acb5e826d5b288cd758bed7cd4
Gabriel Hora <gabrielhoradeveloper@gmail.com>
2023-03-02 20:47:52 -0300

    fix gradle and setup action bar

--- app/src/main/java/com/rodrigoapolo/gogarage/MainActivity.kt
+++ app/src/main/java/com/rodrigoapolo/gogarage/MainActivity.kt
@@ -2,13 +2,15 @@ package com.rodrigoapolo.gogarage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rodrigoapolo.gogarage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        supportActionBar?.hide()
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        return setContentView(binding.root)
     }
 }
