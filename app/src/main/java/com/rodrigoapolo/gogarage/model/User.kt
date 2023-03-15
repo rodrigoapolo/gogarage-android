b7b1f775f37d8a98a0cc530df915d2a84a2d3685
rodrigoapolo <rodrigoapolodev@gmail.com>
2023-03-15 20:32:02 -0300

    add icone no app

--- app/src/main/java/com/rodrigoapolo/gogarage/model/User.kt
+++ app/src/main/java/com/rodrigoapolo/gogarage/model/User.kt
@@ -1,7 +1,5 @@
 package com.rodrigoapolo.gogarage.model
 
-import android.os.Parcel
-import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class User(
@@ -21,41 +19,4 @@ data class User(
     var cpf: String?,
     @SerializedName("cnpj")
     var cnpj: String?,
-    ): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readValue(Long::class.java.classLoader) as? Long,
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readByte() != 0.toByte(),
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeValue(id)
-        parcel.writeString(name)
-        parcel.writeString(email)
-        parcel.writeString(password)
-        parcel.writeString(phone)
-        parcel.writeByte(if (status) 1 else 0)
-        parcel.writeString(cpf)
-        parcel.writeString(cnpj)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<User> {
-        override fun createFromParcel(parcel: Parcel): User {
-            return User(parcel)
-        }
-
-        override fun newArray(size: Int): Array<User?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
